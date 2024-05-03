d3e2c05a30c2f7998ca70d8fa69c0bc793de13c3
sicalo330 <noah.esteban9@gmai.com>
2024-05-03 10:43:18 -0500

    COmentario buenardo

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
 console.log("Hello world")
 console.log("Second hello world")
+
+//Para ignorar los archivos es necesario crear un gitignore enla raiz del proyecto y poner lo siguiente
+//      /nombreDelArchivo
