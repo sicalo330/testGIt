bc9147e033c1e75202e6ed9ad49c6f0452e9476d
sicalo330 <noah.esteban9@gmai.com>
2024-05-03 10:46:51 -0500

    ultimo comentario

--- test.js
+++ test.js
@@ -3,3 +3,6 @@ console.log("Second hello world")
 
 //Para ignorar los archivos es necesario crear un gitignore enla raiz del proyecto y poner lo siguiente
 //      /nombreDelArchivo
+
+
+//Otro comenario
